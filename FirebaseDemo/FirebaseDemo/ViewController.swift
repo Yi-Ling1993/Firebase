186608c66481d03120f5e66cd36a21c2d57b4073
Liu Yi-Ling <liuyiling1993@gmail.com>
2018-09-04 13:49:48 +0800

    篩選特定人的文章及tag(包含雙重條件)

--- FirebaseDemo/FirebaseDemo/ViewController.swift
+++ FirebaseDemo/FirebaseDemo/ViewController.swift
@@ -11,6 +11,20 @@ import FirebaseDatabase
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var titleText: UITextField!
+    @IBOutlet weak var tagText: UITextField!
+    @IBOutlet weak var contentText: UITextField!
+    
+    var email: String = ""
+    var name: String = ""
+    var titleField: String? = ""
+    var tag: String = ""
+    var content: String? = ""
+    
+    
     var ref: DatabaseReference!
     
     override func viewDidLoad() {
@@ -18,19 +32,87 @@ class ViewController: UIViewController {
         
         ref = Database.database().reference()
         
-        self.ref.child("user").child("user101").setValue(["email": "test2222@gmail.com", "friends": ["user01", "user02", "user03"], "name": "John Doe"])
+//        removeData()
+        
+      
+        
+//        seeData()
+        
+        filter()
+        
+
+    }
+    
+    func removeData() {
+        
+        self.ref.child("user").child("user02").removeValue()
+    }
+    
+    
+    @IBAction func sendArticle(_ sender: Any) {
+        
+        createArticle()
+    }
+    
+    func createArticle() {
         
         let key = ref.child("article").childByAutoId().key
-        let post = ["content": "我想回家",
-                    "posted_by": "user01",
-                    "tag": "beauty"]
+        print(key)
+        
+        let now: Date = Date()
+        let dateFormat: DateFormatter = DateFormatter()
+        dateFormat.dateFormat = "MM/dd/yyyy HH:mm:ss"
+        let dateString: String = dateFormat.string(from: now)
+        
+        content = contentText.text
+        tag = tagText.text!
+        titleField = titleText.text!
+        
+        
+        let post = ["article_title": titleField,
+                    "article_content": content,
+                    "author": email,
+                    "article_tag": tag,
+                    "author_tag": "\(email)_\(tag)",
+                    "created_time": dateString]
         
         let childUpdates = ["/article/\(key)": post]
         
         ref.updateChildValues(childUpdates)
+    }
+    
+    @IBAction func goUser(_ sender: Any) {
         
-        ref.observe(.value) { (snapshot) in
+        createUser()
+    }
+    
+    func createUser() {
+        
+        let key = ref.child("user").childByAutoId().key
+        
+        email = emailText.text!
+        name = nameText.text!
+        
+        let post = ["email": email,
+                    "friends": "",
+                    "username": name]
+        
+        let childUpdates = ["/user/\(key)": post]
+        ref.updateChildValues(childUpdates)
+    }
+    
+    func seeData() {
+        
+        ref.child("article").observe(.value) { (snapshot) in
             print(snapshot)
+        }
+    }
+    
+    func filter() {
+        
+        ref.child("article").queryOrdered(byChild: "author_tag").queryEqual(toValue: "123456@gmail.com_表特").observeSingleEvent(of: .value) { (snapshot) in
+            print(snapshot)
+            
         }
     }
 
